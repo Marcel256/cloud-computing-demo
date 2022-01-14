082ebdcb3658a8943b1ac03467f8d26c5d663402
marcel.brunnbauer@gmx.at <marcel.brunnbauer@gmx.at>
2022-01-14 10:25:03 +0100

    Initial Commit

---
+++ src/test/kotlin/cloud/team4/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package cloud.team4.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
